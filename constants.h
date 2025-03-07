7267b71111492f65c47cca5f20c774e49757da43
Ochihai Omuha <113269120+oomuh570@users.noreply.github.com>
2025-03-07 00:20:04 -0700

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,6 +1,6 @@
 /*
     MAX_PARTICIPANTS: The maximum number of participants (teams) allowed in the tournament.
-    MAX_MATCHUPS: The maximum number of matchups (games) that can be scheduled for the tournament./
+    MAX_MATCHUPS: The maximum number of matchups (games) that can be scheduled for the tournament.
  */ 
 
   #define MAX_PARTICIPANTS 100
