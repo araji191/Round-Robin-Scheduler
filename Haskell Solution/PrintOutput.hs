a3137273100850855120dc54db86e5c88a43be41
oomuh750 <oomuh570@mtroyal.ca>
2025-04-04 14:16:45 -0600

    test drivers

---
+++ Haskell Solution/PrintOutput.hs
@@ -0,0 +1,58 @@
+module PrintOutput 
+(
+    compareMatches, 
+    printMatch, 
+    printSchedule
+) where
+
+import Time
+import Match
+import Data.List (sortBy, groupBy)
+import Data.Function (on)
+import Text.Printf (printf)
+
+
+-- Comparison function using the provided accessor functions.
+
+compareMatches :: Match.Match -> Match.Match -> Ordering
+compareMatches a b =
+    compare (Match.getDay a) (Match.getDay b) <>
+    compare (Match.getVenue a) (Match.getVenue b) <>
+    compare (Time.getHour (Match.getStartTime a)) (Time.getHour (Match.getStartTime b)) <>
+    compare (Time.getMinute (Match.getStartTime a)) (Time.getMinute (Match.getStartTime b))
+
+-- Print a single match with formatted times.
+
+printMatch :: Match.Match -> IO ()
+printMatch m =
+    printf " %02d:%02d-%02d:%02d: %s vs %s\n"
+        (Time.getHour (Match.getStartTime m))
+        (Time.getMinute (Match.getStartTime m))
+        (Time.getHour (Match.getEndTime m))
+        (Time.getMinute (Match.getEndTime m))
+        (Match.getParticipant1 m)
+        (Match.getParticipant2 m)
+
+-- Print the schedule by grouping matches by day and then by venue.
+
+printSchedule :: [Match.Match] -> IO ()
+printSchedule matches = do
+    let sortedMatches = sortBy compareMatches matches
+        days = groupBy ((==) `on` Match.getDay) sortedMatches
+    mapM_ printDayGroup days
+  where
+    printDayGroup :: [Match.Match] -> IO ()
+    printDayGroup ms@(m:_) = do
+        putStrLn $ "\nDAY " ++ show (Match.getDay m) ++ ":"
+        let venues = groupBy ((==) `on` Match.getVenue) ms
+        mapM_ printVenueGroup venues
+    printDayGroup [] = return ()
+
+printVenueGroup :: [Match.Match] -> IO ()
+printVenueGroup ms@(m:_) = do
+    putStrLn $ "Venue " ++ show (Match.getVenue m) ++ ":"
+    mapM_ printMatch ms
+printVenueGroup [] = return ()
+
+
+
---
+++ Haskell Solution/TestConstants.hs
@@ -0,0 +1,21 @@
+import Constants
+
+runConstantTests :: IO ()
+runConstantTests = do
+    putStrLn " "
+    putStrLn "Testing Constants..."
+    putStrLn $ "maxParticipants: " ++ show maxParticipants
+    putStrLn $ "maxMatchups: " ++ show maxMatchups
+    putStrLn $ "maxBacktracks: " ++ show maxBacktracks
+    putStrLn $ "maxVenues: " ++ show maxVenues
+    putStrLn $ "maxDays: " ++ show maxDays
+    putStrLn $ "maxTournamentType: " ++ show maxTournamentType
+    putStrLn $ "minParticipants: " ++ show minParticipants
+    putStrLn $ "maxMatchLengthMinutes: " ++ show maxMatchLengthMinutes
+    putStrLn $ "maxRestPeriodMinutes: " ++ show maxRestPeriodMinutes
+    putStrLn " "
+    putStrLn "All constants are set correctly."
+    putStrLn " "
+    putStrLn "Testing Constants completed."
+    putStrLn " "
+    putStrLn "----------------------------------------"
