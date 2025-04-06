a2ed53ebc1c0642b7cf58c37a5e6cfd7a81091e5
araji191 <113402781+araji191@users.noreply.github.com>
2025-04-06 11:22:36 -0600

    Delete Haskell Solution/test_time.hs

--- Haskell Solution/test_time.hs
+++
@@ -1,7 +0,0 @@
-import Time
-
--- Test function to check if the time interval is valid
-testTime :: Int
-testTime = do 
-            getInterval (newTime 9 30) (newTime 10 45)
-            getInterval (newTime 9 30) (newTime 10 45)
